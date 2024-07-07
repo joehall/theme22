23fce378932d7ee501211173ad4ecfca8fc3862a
Joe Hall <jozhall@gmail.com>
2024-07-07 14:52:27 -0400

    Registered JS

--- functions.php
+++ functions.php
@@ -5,4 +5,27 @@ include 'inc/wp_bootstrap_navwalker.php';
 
 register_nav_menu('main-menu', 'Main menu');
 
+
+
+
+// Register style sheets and scripts.
+add_action( 'wp_enqueue_scripts', 'register_theme_styles' );
+add_action( 'wp_enqueue_scripts', 'register_theme_scripts' );
+
+
+function register_theme_styles() {
+    wp_register_style( 'bootstrapstylesheet', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css' );
+    wp_enqueue_style( 'bootstrapstylesheet' );
+}
+
+
+function register_theme_scripts() {
+	$args = array('strategy'  => 'defer','in_footer'  => true);
+    wp_enqueue_script( 'bootstrapstylescripts', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js', array(), '5.3.3', $args);
+    ##wp_enqueue_style( 'bootstrapstylescripts' );
+}
+
+
+
+
 ?>
--- header.php
+++ header.php
@@ -4,7 +4,6 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php wp_title( '|', true, 'right' ); ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
 <?php wp_head(); ?>
   </head>
